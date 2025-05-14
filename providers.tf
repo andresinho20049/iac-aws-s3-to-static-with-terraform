35c5165f3348dbe8762d834052e99efd89a03632
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-05-14 17:37:27 -0300

    feat: Update AWS provider version and add region variable for flexibility

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.97.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
   shared_config_files = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
 }
--- variables.tf
+++ variables.tf
@@ -14,3 +14,9 @@ variable "goal" {
   description = "The goal of the deployment (e.g., static-site, api)"
   type        = string
 }
+
+variable "region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-east-1"
+}
