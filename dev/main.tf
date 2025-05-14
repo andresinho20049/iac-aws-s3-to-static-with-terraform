4146d327a1de4d8e40c7e97d372ee54963f2a80e
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-05-14 17:03:13 -0300

    feat: Add Terraform configuration for development environment and S3 bucket output

---
+++ dev/main.tf
@@ -0,0 +1,12 @@
+module "development" {
+  source = "../"
+  account_username = "andresinho20049"
+  environment = "dev"
+  goal = "lovable-spa-test"
+}
+
+output "s3_dns" {
+  value = module.homolog.s3_dns
+  description = "The DNS name of the S3 bucket for static website hosting"
+  
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "account_username" {
+  description = "The AWS account username"
+  type        = string
+  nullable = false
+}
+
+variable "environment" {
+  description = "The environment for the deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "goal" {
+  description = "The goal of the deployment (e.g., static-site, api)"
+  type        = string
+}
